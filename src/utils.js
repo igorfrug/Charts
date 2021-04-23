1c017e373517b7f466bfd6d63826dac1e45966b6
Igor <igorfrug@gmail.com>
2021-04-23 19:44:47 +0300

    updated version

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
+//Parsing data from API
 function parseData(data) {
 	const newLocal = [];
 	let dArr = newLocal
-	let date=new Date(data.time_now * 1000)
+	let date = new Date(data.time_now * 1000)
 	console.log(data.time_now)
 	for (let d of data.result) {
 		//for real time data
@@ -13,23 +14,22 @@ function parseData(data) {
 		d.low = +d.low;
 		d.close = +d.close;
 		d.volume = +d.volume;
-		d.turnover=+d.turnover
-		dArr.push(d)
+		d.turnover = +d.turnover;
+		dArr.push(d);
 	};
-
-	return dArr
+	return dArr;
 }
-
+// Call API
 export async function getData() {
 	try {
 		let res = await fetch(`https://api.bybit.com/public/linear/kline?symbol=BTCUSDT&interval=1&limit=200&from=${Math.floor(Date.now() / 1000 - 11700)}`);
 		const data = await res.json();
-		console.log(data)
+		console.log(data);
 		//for real time data
-		if (data.result.length>1) {
+		if (data.result.length > 1) {
 			const promiseMSFT = parseData(data);
 			//for static data
-			//const promiseMSFT=parseData(data)
+			//const promiseMSFT=parseData(data);
 			return promiseMSFT;
 		}
 	} catch (err) {
